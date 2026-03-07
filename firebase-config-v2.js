076c6782c0e40ca59ce0a471f5a0e056300b279c
Milanjyoti Ray <milanjyotiray84@gmail.com>
2026-03-07 20:40:50 +0530

    perf: ultra-low latency architecture using global firebase cache and binary blob transmission

--- firebase-config-v2.js
+++ firebase-config-v2.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
 import { getFirestore, collection, addDoc, getDoc, getDocs, doc, setDoc, deleteDoc, onSnapshot, increment } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
-import { getStorage, ref, uploadString, getDownloadURL, deleteObject } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-storage.js";
+import { getStorage, ref, uploadString, uploadBytes, getDownloadURL, deleteObject } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-storage.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDWWTKONpQbfFIYUUcG9AsJy1Xg6X_GCds",
@@ -18,4 +18,4 @@ const db = getFirestore(app);
 const auth = getAuth(app);
 const storage = getStorage(app);
 
-export { app, db, auth, storage, collection, addDoc, getDoc, getDocs, doc, setDoc, deleteDoc, onSnapshot, increment, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged, GoogleAuthProvider, signInWithPopup, ref, uploadString, getDownloadURL, deleteObject };
+export { app, db, auth, storage, collection, addDoc, getDoc, getDocs, doc, setDoc, deleteDoc, onSnapshot, increment, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged, GoogleAuthProvider, signInWithPopup, ref, uploadString, uploadBytes, getDownloadURL, deleteObject };
